77c4e7e260a8328cad9fa2a04bf3906f58d88541
Ankush263 <ankushbanik263@gmail.com>
2022-12-03 16:55:15 +0530

    update

--- client/pages/components/Marketplace/SideBar.tsx
+++ client/pages/components/Marketplace/SideBar.tsx
@@ -3,14 +3,16 @@ import React, { useState, useEffect } from 'react'
 import PersonIcon from '@mui/icons-material/Person';
 import { ethers } from 'ethers';
 import artifacts from "../../../utils/Ecommarce.json";
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 
 function SideBar() {
 
-  const deployedAddress: string = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+  const deployedAddress: string = "0xaeBf6b98F358aE5449fABe2Bcb83f1754eE40FdD"
   const ABI: any = artifacts.abi
 
   const [walletAddress, getWalletAddress] = useState('')
   const [walletBalance, getWalletBalance] = useState('')
+  const [USD, setUSD] = useState('0')
 
   const getAddress = async () => {
     try {
@@ -19,10 +21,15 @@ function SideBar() {
         const signer: any = await provider.getSigner()
         const Address: string = await signer.getAddress()
         const contract: any = await new ethers.Contract(Address, ABI, signer)
+        // let usd = await contract.getLatestPrice()
         const Balance = await provider.getBalance(Address)
         const balance = await ethers.utils.formatEther(Balance)
         const addr = Address.slice(0, 5)+ "..." + Address.slice(38, 42)
         const bal = balance.slice(0, 8)
+        // console.log(Number(bal))
+        let usd = (Number(bal) * 0.92).toString()
+        usd = usd.slice(0, 7)
+        setUSD(usd)
         getWalletAddress(addr)
         getWalletBalance(bal)
       }
@@ -57,7 +64,7 @@ function SideBar() {
             <span className={styles.accountInfo}>{walletAddress}</span>
             <span className={styles.accountInfo}>
               {walletBalance}
-              <img src="/images/eth.png" className='w-5' />
+              <img src="/images/polygon.png" className='w-7 ml-1' />
             </span>
           </div>
         </div>
@@ -68,11 +75,12 @@ function SideBar() {
           <span>{walletAddress}</span>
         </div>
         <div className={styles.sideWallet}>
-          <img src="/images/eth.png" className='w-8' />
+          <img src="/images/polygon.png" className='w-8' />
           <span>{walletBalance}</span>
         </div>
         <div className={styles.sideWallet}>
-          <PersonIcon fontSize='large' />
+          <AttachMoneyIcon fontSize='large' className='text-blue-600' />
+          <span className='text-white font-bold'>${USD} USD</span>
         </div>
         <div className={styles.sideWallet}>
           <img src='/images/ecommerce-logo.png' className='w-16 shadow-2xl border-white-900/75 rounded-full' />
